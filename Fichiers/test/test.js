ee44f552f92fad3b74e7ed35b939f09c7d4e1037
UGOAT <ugodpc2@gmail.com>
2026-03-18 17:36:46 +0100

    premiers tests

---
+++ Fichiers/test/test.js
@@ -0,0 +1,53 @@
+import { Pokemon } from "../pokemon.js";
+import { Attack } from "../attack.js";
+
+function getPokemonByType(typeName)
+{
+    const tablo_pokemon = Object.values(Pokemon.all_pokemons);
+    const res = tablo_pokemon.filter((pokemon) => pokemon._types_pokemon.some(type => type.type === typeName));
+
+    console.log(`Liste des ${res.length} Pokemons :`);
+    res.forEach(res => {
+        console.log(`- ${res.toString()}`);
+    });
+    return res;
+}
+
+function getPokemonByAttack(attackName)
+{
+
+}
+
+function getAttacksByType(typeName) {
+    let typeLower = typeName.toLowerCase();
+    let attacks = Object.values(Attack.all_attacks).filter(a => a.type_attack && a.type_attack.toLowerCase() === typeLower);
+    
+    console.log(`Liste des ${attacks.length} Attaques :`);
+    attacks.forEach(attack => {
+        console.log(`- ${attack.toString()}`);
+    });
+}
+
+function sortPokemonByTypeThenName()
+{
+
+}
+
+function getWeakestEnnemies(attackName)
+{
+
+}
+
+function getBestFastAttacksForEnemy(print, pokemonName)
+{
+
+}
+
+function fastFight(pokemonNameA, pokemonNameB)
+{
+
+}
+
+//console.table(getPokemonByType("Water"));
+getPokemonByType("Water");
+getAttacksByType("Bug");
