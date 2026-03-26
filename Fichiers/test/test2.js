32994b088fb511d4c18c38e1a55a032d42e6a02b
lhurtrel <lhurtrel@pce-test>
2026-03-26 11:25:21 +0100

    fonction sort 1

--- Fichiers/test/test2.js
+++ Fichiers/test/test2.js
@@ -2,8 +2,26 @@ import { Pokemon } from "../pokemon.js";
 
 function sortPokemonByTypeThenName()
 {
-    let pokemons = Pokemon.all_pokemons();
-    pokemons[pokemon_name].sort();
-    return pokemons;
+    const pokemonsList = Object.values(Pokemon.all_pokemons);
+
+    pokemonsList.sort((a, b) =>
+    {
+        const nomsTypesA = a.getTypes().map(t => t.type).sort((x, y) => x.localeCompare(y, 'fr'));
+        const typeKeyA = nomsTypesA.join('-');
+
+        const nomsTypesB = b.getTypes().map(t => t.type).sort((x, y) => x.localeCompare(y, 'fr'));
+        const typeKeyB = nomsTypesB.join('-');
+
+        const cmpTypes = typeKeyA.localeCompare(typeKeyB, 'fr');
+        if (cmpTypes !== 0) return cmpTypes;
+
+        return a._nom_pokemon.localeCompare(b._nom_pokemon, 'fr');
+    });
+
+    for (let pokemon of pokemonsList)
+    {
+        console.log(pokemon.toString());
+    }
 }
 
+console.log(sortPokemonByTypeThenName());
